13f56955d758b824f1d7633181526fec2db6834d
Shaik Shafi Ahmed <56587592+shafiahmed12@users.noreply.github.com>
2026-06-12 17:16:28 +0530

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public bool hasDuplicate(int[] nums) {
+        var set = new HashSet<int>();
+
+        for(int i = 0; i < nums.Length; i++)
+        {
+            if(set.Contains(nums[i]))
+            {
+                return true;
+            }
+            set.Add(nums[i]);
+        }
+        return false;
+    }
+}
