3254adb92f06ad0bd6cecb824eb57f503da1ec1b
Shaik Shafi Ahmed <56587592+shafiahmed12@users.noreply.github.com>
2026-07-11 23:06:53 +0530

    Add: remove-duplicates-from-sorted-array - submission-0

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-0.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int RemoveDuplicates(int[] nums) {
+        var left = 1;
+
+        for(int right = 1; right < nums.Length; right++)
+        {
+            if(nums[right] != nums[right-1])
+            {
+                nums[left] = nums[right];
+                left++;
+            }
+        }
+
+        return left;
+    }
+}
